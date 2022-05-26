6fad0d95474129069ff9e73bc6ab9f724babe373
Kidus Guade <kidusguade41@gmail.com>
2022-05-25 21:42:58 -0700

    finish practice django drf

--- watchlist/api/serializers.py
+++ watchlist/api/serializers.py
@@ -1,3 +1,64 @@
+from wsgiref.validate import validator
 from rest_framework import serializers
+from watchlist.models import Movie
 
-class 
+class MovieSerializer(serializers.ModelSerializer):
+    len_name = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = Movie
+        fields = "__all__"
+        
+    def get_len_name(self, object):
+        return len(object.name)
+    
+    def validate(self, data):
+        if data['name'] == data['description']:
+            raise serializers.ValidationError("name and description must be different")
+        else:
+            return data
+    def validate_name(self, value):
+        
+        if len(value) < 2:
+            raise serializers.ValidationError("name is too short")
+        else:
+            return value
+        
+
+# def name_length(value):
+#     if len(value) < 2:
+#         raise serializers.ValidationError("this name is fucking short!!!!!!!!!!!")
+    
+
+# class MovieSerializer(serializers.Serializer):
+#     id = serializers.IntegerField(read_only=True)
+#     name = serializers.CharField(validators=[name_length])
+#     description = serializers.CharField()
+#     active = serializers.BooleanField()
+    
+#     def create(self, validated_data):
+#         return Movie.objects.create(**validated_data)
+    
+#     def update(self, instance, validated_data):
+        
+#         instance.name = validated_data.get('name', instance.name)
+#         instance.description = validated_data.get('description', instance.description)
+#         instance.active = validated_data.get('active',instance.active)
+#         instance.save()
+        
+#         return instance
+    
+#     def validate(self, data):
+#         if data['name'] == data['description']:
+#             raise serializers.ValidationError("name and description must be different")
+#         else:
+#             return data
+#     def validate_name(self, value):
+        
+#         if len(value) < 2:
+#             raise serializers.ValidationError("name is too short")
+#         else:
+#             return value
+        
+    
+    
--- watchlist/api/urls.py
+++ watchlist/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from watchlist.api import views
+from watchlist.api.views import MovieListAV, MovieDetailAv
 
 urlpatterns = [
-   
+   path('list/', MovieListAV.as_view(), name="movie_list"),
+   path('<int:pk>/', MovieDetailAv.as_view(), name="movie")
 ]
